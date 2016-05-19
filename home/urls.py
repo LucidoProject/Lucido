9f7af38d8661002423017c74a777433e81de343b
root <root@edu-x04.edu.marconi>
2016-05-19 10:35:25 +0200

    Pages and infog.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'infographics', views.infographics, name='infographics'),
 	url(r'^$', views.opendata, name='opendata'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from .models import OpenData
 def opendata(requests):
 	sheets = OpenData.objects.all()
 	return render(requests, 'index.html', {'sheets': sheets})
+
+def infographics(requests):
+	return render(requests, 'infographics.html')
